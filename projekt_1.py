71226853fae8e63f7bcf677a948a28d8604a600d
Yehor Baranov <yhr.baranov@post.cz>
2024-11-10 13:24:32 +0100

    Přihlášení uživatelů

--- projekt_1.py
+++ projekt_1.py
@@ -18,4 +18,30 @@ seznam_slov_2 = list(filter(None, re.split(r'\W+', text_2.lower())))
 seznam_slov_3 = list(filter(None, re.split(r'\W+', text_3.lower())))
 
 
-# seznam zaregistrovaných uživatelů
+# Přihlášení uživatelů
+users_registred = {"bob":"123", "ann":"pass123","mike":"password123", "liz":"pass123"}
+
+print(f"Sign in:")
+name = input("name: ")
+
+# Kontrola, zda je uživatel registrován
+if name.lower() not in users_registred.keys():
+    print("unregistered user, terminating the program.")
+    exit(input("Press <Enter> to exit..."))
+# Ověření hesla s možností dalších pokusů
+else:
+    attempts = 3  # Celkový počet pokusů
+    while attempts > 0:
+        password = input("password: ")
+        if password == users_registred[name.lower()]:
+            break
+        else:
+            attempts -= 1
+            if attempts > 0:
+                print(f"Incorrect password. You have {attempts} attempt(s) left.")
+            else:
+                print("Access denied. Terminating the program.")
+                exit(input("Press <Enter> to exit..."))
+
+
+# Výběr textu k analýze
