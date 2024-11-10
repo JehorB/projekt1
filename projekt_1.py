6246154c3acca9e9d5221635190c0bf4c2868fd5
Yehor Baranov <yhr.baranov@post.cz>
2024-11-10 09:27:25 +0100

    Stvoreni seznamu textu

--- projekt_1.py
+++ projekt_1.py
@@ -5,3 +5,17 @@ author: Yehor Baranov
 email: yhr.baranov@post.cz
 
 """
+import re
+from task_template import TEXTS # import textů
+
+
+# stvorení seznamu slov
+text_1 = TEXTS[0]
+text_2 = TEXTS[1]
+text_3 = TEXTS[2]
+seznam_slov_1 = list(filter(None, re.split(r'\W+', text_1.lower())))
+seznam_slov_2 = list(filter(None, re.split(r'\W+', text_2.lower())))
+seznam_slov_3 = list(filter(None, re.split(r'\W+', text_3.lower())))
+
+
+# seznam zaregistrovaných uživatelů
