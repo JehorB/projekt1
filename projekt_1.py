2f38cd2c18c7f9a757b1169c501d5647a5079490
Yehor Baranov <yhr.baranov@post.cz>
2024-11-11 23:04:07 +0100

    Analýza textu podle výběru

--- projekt_1.py
+++ projekt_1.py
@@ -8,6 +8,8 @@ email: yhr.baranov@post.cz
 import re
 from task_template import TEXTS # import textů
 
+separator = '-' * 40 # dělící čára
+
 
 # stvorení seznamu slov
 text_1 = TEXTS[0]
@@ -26,11 +28,14 @@ users_registred = {
     "liz":"pass123"
 }
 
+print(separator) # dělící čára
+
 print(f"Sign in:")
 name = input("name: ").lower().strip()
 
 # Kontrola, zda je uživatel registrován
 if name not in users_registred.keys():
+    print(separator)
     print("unregistered user, terminating the program.")
     for _ in range(10**8):  # Zpoždění v prázdném cyklu
         pass
@@ -48,10 +53,13 @@ else: # Ověření hesla s možností dalších pokusů
                 f"Incorrect password. You have {attempts} attempt(s) left."
                 )
             else:
+                print(separator)
                 print("Access denied. Terminating the program.")
                 for _ in range(10**8):  # Zpoždění v prázdném cyklu
                     pass
                 exit()
+
+print(separator) # dělící čára
 
 print(f"Welcome to the app, {name.capitalize()}")
 
@@ -59,12 +67,19 @@ print(f"Welcome to the app, {name.capitalize()}")
 # Výběr textu k analýze
 print("We have 3 texts to be analyzed.")
 
+print(separator) # dělící čára
+
 nomber_text = input("Enter a number btw. 1 and 3 to select: ")
+
+print(separator) # dělící čára
 
 # Kontrola zadání čísla textů
 if nomber_text.isdigit():
     if nomber_text not in ("1", "2", "3"):
-        print("The selected text number is not valid. Terminating the program.")
+        print(
+            "The selected text number is not valid. "
+            "Terminating the program."
+        )
         for _ in range(10**8):  # Zpoždění v prázdném cyklu
             pass
         exit()
@@ -73,5 +88,34 @@ else:
     for _ in range(10**8):  # Zpoždění v prázdném cyklu
         pass
     exit()
+
+
+# Analýza textu podle výběru
+text_selected = TEXTS[int(nomber_text) - 1] # Výběr textu ze seznamu
+
+# Vyčištění textu od mezer a interpunkčních znamének
+text_list = list(filter(None, re.split(r'\W+', text_selected)))
+# Nalezení a počítání titulních slov
+word_titlecase = sum(1 for word in text_list if word.istitle())
+# Nalezení a počítání slov psaných velkými písmeny
+word_uppercase = sum(1 for word in text_list 
+                     if word.isupper() and word.isalpha()
+                     )
+# Nalezení a počítání slov psaných malými písmeny
+word_lowercase = sum(1 for word in text_list if word.islower())
+# Nalezení a počítání čísel
+numbers = sum(1 for number in text_list if number.isdigit())
+numbers_sum = 0
+for number in text_list:
+    if number.isdigit():
+        number = int(number)
+        numbers_sum += number
+
+print(f"There are {len(text_list)} words in the selected text.")
+print(f"There are {word_titlecase} titlecase words.")
+print(f"There are {word_uppercase} uppercase words.")
+print(f"There are {word_lowercase} lowercase words.")
+print(f"There are {numbers} numeric strings.")
+print(f"The sum of all the numbers {numbers_sum}")
 
 input()
